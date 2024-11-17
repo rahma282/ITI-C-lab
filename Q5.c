96d2a2e1931364425f285eb07bf32ff301cc65aa
rahma282 <rahmamostaf282@gmail.com>
2024-11-18 01:29:39 +0200

    init commit

---
+++ Q5.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int getDesendingMulti()
+{
+
+    int num =0;
+    printf("Descending order:\n");
+   for (int i=10; i>=1; i--){
+        for (int j=10; j>=1; j--){
+            num =i*j;
+            printf ("%d * %d= %d\n" , i,j,num);
+        }
+
+   }
+   return 0;
+
+
+}
