f6a5a5ead71ca9225b7e5df39e368181fcb41120
Rahma Mostafa <136733416+rahma282@users.noreply.github.com>
2024-11-30 10:42:50 +0200

    main.c

--- LABS/Lab4_Matrix/main.c
+++ LABS/Lab4_Matrix/main.c
@@ -2,77 +2,62 @@
 #include <stdlib.h>
 #define ROW 3
 #define COL 4
+
 void print();
 
-int main()
-{
-    int sumOfEveryRows=0,sumOfCols=0;
+int main() {
+    int matrix[ROW][COL];
+    int sumOfEveryRows = 0, sumOfCols = 0;
     int value;
     char ch;
-    int matrix[ROW][COL];
-    for (int i = 0; i < ROW; i++)
-    {
-    for (int j = 0; j < COL; j++)
-        {
-             printf("Enter element [%d,%d]: ", i, j);
-<<<<<<< HEAD
 
-                 while (scanf("%d%c", &value, &ch) != 2 || ch != '\n') {
-                // Clear the input buffer of any invalid data
-=======
-<<<<<<< HEAD
-             while (scanf("%d%c", &value, &ch) != 2 || ch != '\n') {  
-                // Clear the input buffer of any invalid data  char or num+char 
-=======
 
-                 while (scanf("%d%c", &value, &ch) != 2 || ch != '\n') {
-                // Clear the input buffer of any invalid data
->>>>>>> 4674bac (update menu)
->>>>>>> ed986b0 (update DBMenu)
-                while (getchar() != '\n');
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            printf("Enter element [%d,%d]: ", i, j);
+
+
+            while (scanf("%d%c", &value, &ch) != 2 || ch != '\n') {
+                while (getchar() != '\n');  // Clear the input buffer
                 printf("Invalid input! Please enter a valid integer: ");
             }
             matrix[i][j] = value;
         }
-
     }
-        printf("\n");
 
+    printf("\nMatrix Elements:\n");
+    print();
+    for (int i = 0; i < ROW; i++) {
+        for (int j = 0; j < COL; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    }
     print();
 
-    for (int i = 0; i < ROW; i++)
-    {
-    for (int j = 0; j < COL; j++)
-        {
-             printf("%d\t", matrix[i][j]);
+    // cal row sums
+    for (int i = 0; i < ROW; i++) {
+        sumOfEveryRows = 0;
+        for (int j = 0; j < COL; j++) {
+            sumOfEveryRows += matrix[i][j];
         }
-        printf("\n");
+        printf("Sum of row %d: %d\n", i, sumOfEveryRows);
     }
-   print();
+    print();
 
-    for (int i = 0; i < ROW; i++)
-    {
-        sumOfEveryRows=0;
-        for (int j = 0; j < COL; j++)
-        {
-            sumOfEveryRows+=matrix[i][j];  //sum of every rows
-    }
-        printf("sum of row %d: %d\n",i,sumOfEveryRows);
-        print();
-    }
-    for (int j=0; j<COL; j++){
-            sumOfCols=0;
-        for (int i=0; i<ROW; i++){
-            sumOfCols+=matrix[i][j];  //sum of columns
-
+    // cal column averages
+    for (int j = 0; j < COL; j++) {
+        sumOfCols = 0;
+        for (int i = 0; i < ROW; i++) {
+            sumOfCols += matrix[i][j];
         }
         //printf("sum of col %d: %d\n",j,sumOfCols);
-        printf("average of column %d:%d\n",j,sumOfCols/ROW);  //average of cols
+        printf("Average of column %d: %.2f\n", j, (float)sumOfCols / ROW);
     }
 
     return 0;
 }
 
-void print (){
+void print() {
     printf("--------------------------\n");
 }
