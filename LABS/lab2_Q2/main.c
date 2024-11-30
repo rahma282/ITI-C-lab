5f79fb521fa5a95bcf6b92040484e8e3d81d6353
Rahma Mostafa <136733416+rahma282@users.noreply.github.com>
2024-11-30 09:40:31 +0200

    main.c

--- LABS/lab2_Q2/main.c
+++ LABS/lab2_Q2/main.c
@@ -3,12 +3,28 @@
 
 int main()
 {
-     int num1=0 , sum =0;
-    while(sum <=100){
-    printf("input a number: ");
-    scanf (" %d", &num1);
-    sum += num1;
+    int n =0, sum =0, vaildInput;
+
+    printf("enter number: ");
+    while (1){
+    vaildInput =scanf("%d",&n);
+    if (vaildInput !=1){
+        printf("invalid input , please input valid integer:\n ");
+        while (getchar() != '\n'); // Clear the input buffer
     }
-    return 0;
+    else if (vaildInput < 0){
+        printf("invalid input , please input positive integer only:\n ");
+    }
+    else{
+        sum += n;
+    }
+    if (sum >100){
+        printf("sum exceeded 100! Final sum: %d\n", sum);
+        return;
+    }
+    printf("%d\n",sum);
     }
 
+
+    return 0;
+}
